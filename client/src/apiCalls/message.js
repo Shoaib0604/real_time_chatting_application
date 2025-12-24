d61aa83c3abf70ab30775e2b5a5876c9435b7792
Bagwan Shoaib <bagwanshoaib0013@gmail.com>
2025-12-24 16:37:21 +0530

    done

--- client/src/apiCalls/message.js
+++ client/src/apiCalls/message.js
@@ -1,10 +1,9 @@
 import { axiosInstance } from "./index";
-import { url } from "./index";
 
 export const createNewMessage = async (message) => {
   try {
     const response = await axiosInstance.post(
-      url + "/api/message/new-message",
+      "/api/message/new-message",
       message
     );
     return response.data;
@@ -16,7 +15,7 @@ export const createNewMessage = async (message) => {
 export const getAllMessages = async (chatId) => {
   try {
     const response = await axiosInstance.get(
-      url + `/api/message/get-all-messages/${chatId}`
+      `/api/message/get-all-messages/${chatId}`
     );
     return response.data;
   } catch (error) {
--- client/src/apiCalls/users.js
+++ client/src/apiCalls/users.js
@@ -1,9 +1,8 @@
 import { axiosInstance } from "./index";
-import { url } from "./index";
 
 export const getLoggedUser = async () => {
   try {
-    const response = await axiosInstance.get(url + "/api/user/get-logged-user");
+    const response = await axiosInstance.get("/api/user/get-logged-user");
     return response.data;
   } catch (error) {
     return error;
@@ -12,7 +11,7 @@ export const getLoggedUser = async () => {
 
 export const getAllUsers = async () => {
   try {
-    const response = await axiosInstance.get(url + "/api/user/get-all-user");
+    const response = await axiosInstance.get("/api/user/get-all-user");
     return response.data;
   } catch (error) {
     return error;
@@ -21,12 +20,9 @@ export const getAllUsers = async () => {
 
 export const uploadProfilePic = async (image) => {
   try {
-    const response = await axiosInstance.post(
-      url + "/api/user/upload-profile-pic",
-      {
-        image,
-      }
-    );
+    const response = await axiosInstance.post("/api/user/upload-profile-pic", {
+      image,
+    });
     return response.data;
   } catch (error) {
     return error;
